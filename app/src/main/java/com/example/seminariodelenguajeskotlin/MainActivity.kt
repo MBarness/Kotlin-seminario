206ab019471b7f3bdb2e00cd8d0410bf71e5feb7
Martin <maferreyrob252@gmail.com>
2023-08-30 14:08:34 -0300

    toolbar basico

--- app/src/main/java/com/example/seminariodelenguajeskotlin/MainActivity.kt
+++ app/src/main/java/com/example/seminariodelenguajeskotlin/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.example.seminariodelenguajeskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar = findViewById(R.id.toolBar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+
+
+    }
+    //crea las opciones del menu, hacia donde podes ir
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //formato del menu, las opciones
+        menuInflater.inflate(R.menu.menu_main , menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    private fun saludarUsuario() {
-        // Obtengo los datos que me mandaron
-        var bundle: Bundle? = intent.extras
-        // Reviso que efectivamente tenga datos
-        if (bundle != null) {
-            // Obtengo el dato especifico
-            var nombre = bundle?.getString("nombre")
-            var apellido = bundle?.getString("apellido")
-            // Muestro el mensaje
-            Toast.makeText(this, "Bienvenido/a $nombre $apellido", Toast.LENGTH_SHORT).show()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //si el item tocado es igual a algun id de las opciones, madalo a esa pagina
+        if(item.itemId == R.id.itemListado){
+            val intentListado = Intent(this, ListadoPersonajes::class.java)
+            startActivity(intentListado)
+            finish()
         }
+        return super.onOptionsItemSelected(item)
     }
+
 
 }
